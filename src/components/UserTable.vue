81d359b6d1bf6b0708d14d8c29407781ac13006f
keremh <hallackerem@gmail.com>
2019-09-14 23:13:24 +0300

    initial commit

---
+++ src/components/UserTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="user-table">
+    <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+        </tr>
+    </tbody>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'user-table',
+    props: {
+        users: Array,
+    },
+  }
+</script>
+
+<style scoped></style>
